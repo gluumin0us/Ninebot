6116edf9d54b12d7217344e8b5fc360f9a506c92
gluumin0us <168494669+gluumin0us@users.noreply.github.com>
2024-05-01 06:47:05 +0000

    added a cache for characters currently in use; added functions for finding and saving characters; implemented hp modification; printchar() now prints legendary tick marks

--- character.py
+++ character.py
@@ -9,18 +9,28 @@ class Character:
     self.thp = 0
     self.mod = [0, 0, 0, 0, 0, 0, 0]
     self.legendary = [0, 0, 0, 0, 0, 0, 0]
-    self.stats = [2 + self.level + self.legendary[0] + self.mod[0],
-                  2 + self.level + self.legendary[1] + self.mod[1],
-                  2 + self.level + self.legendary[2] + self.mod[2],
-                  2 + self.level + self.legendary[3] + self.mod[3],
-                  2 + self.level + self.legendary[4] + self.mod[4],
-                  2 + self.level + self.legendary[5] + self.mod[5],
-                  2 + self.level + self.legendary[6] + self.mod[6]]
+    self.str = 2 + self.level
+    self.dex = 2 + self.level
+    self.cha = 2 + self.level
+    self.int = 2 + self.level
+    self.att = 2 + self.level
+    self.wil = 2 + self.level
+    self.luc = 2 + self.level
     self.tal = []
     self.effect = []
     self.ss = ""
     self.pt = ["", "", "", ""]
+    self.id = ""
 
   def talisman(self, name: str, stat: int, stat_amount: int):
     self.tal.append(name)
     self.mod[stat] += stat_amount
+
+  def restat(self):
+    self.str += self.legendary[0] + self.mod[0]
+    self.dex += self.legendary[1] + self.mod[1]
+    self.cha += self.legendary[2] + self.mod[2]
+    self.int += self.legendary[3] + self.mod[3]
+    self.att += self.legendary[4] + self.mod[4]
+    self.wil += self.legendary[5] + self.mod[5]
+    self.luc += self.legendary[6] + self.mod[6]
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import discord
 from replit import db
@@ -25,9 +26,16 @@ names = {
 my_char = Character("Simon", 1, 0)
 my_char.legendary[3] += 1
 my_char.legendary[5] += 1
+my_char.id = "262320046653702145"
 
 felix_char = Character("Felix", 2, 20)
 felix_char.legendary[5] += 1
+felix_char.id = names["FELIX"]
+
+mike_char = Character("Michael", 3, 164)
+mike_char.legendary = [1, 0, 1, 0, 1, 2, 0]
+mike_char.talisman("Pearl Necklace", 0, 0)
+mike_char.id = names["MIKEY"]
 
 jack_char = Character("Jack", 9, 1495)
 jack_char.legendary[4] += 2
@@ -37,11 +45,13 @@ jack_char.talisman("Vision's Necklace", 5, 1)
 jack_char.talisman("Wrapped Ribbon", 1, 2)
 jack_char.talisman("Dragon's Bane Armor", 4, 4)
 jack_char.thp = 40
-print(jack_char.stats)
+jack_char.id = names["JACK"]
+
 
 def char_to_list(char: Character):
   return [char.name, char.level, char.xp, char.hp, char.thp, 
-          char.tal, char.effect, char.ss, char.pt, char.mod, char.legendary]
+          char.tal, char.effect, char.ss, char.pt, char.mod, char.legendary, 
+          char.id]
 
 def list_to_char(li_char: list):
   char = Character(li_char[0], li_char[1], li_char[2])
@@ -53,18 +63,24 @@ def list_to_char(li_char: list):
   char.pt = list(li_char[8])
   char.mod = li_char[9]
   char.legendary = li_char[10]
+  char.id = li_char[11]
+  char.restat()
   return char
 
 def printchar(char: Character):
   printable = ""
   printable += (f"__{char.name}: LV{char.level}, {char.xp}/{char.xp_til_next}__\n")
-  printable += (f"Strength - {char.stats[0]}\n")
-  printable += (f"Dexterity - {char.stats[1]}\n")
-  printable += (f"Charisma - {char.stats[2]}\n")
-  printable += (f"Intelligence - {char.stats[3]}\n")
-  printable += (f"Attack - {char.stats[4]}\n")
-  printable += (f"Willpower - {char.stats[5]}\n")
-  printable += (f"Luck - {char.stats[6]}\n")
+  mark = ["", "", "", "", "", "", ""]
+  for i in range(len(char.legendary)):
+    for j in range(char.legendary[i]):
+      mark[i] += '+'
+  printable += (f"{mark[0]}Strength - {char.str}\n")
+  printable += (f"{mark[1]}Dexterity - {char.dex}\n")
+  printable += (f"{mark[2]}Charisma - {char.cha}\n")
+  printable += (f"{mark[3]}Intelligence - {char.int}\n")
+  printable += (f"{mark[4]}Attack - {char.att}\n")
+  printable += (f"{mark[5]}Willpower - {char.wil}\n")
+  printable += (f"{mark[6]}Luck - {char.luc}\n")
   for i in range(len(char.tal)):
     printable += (f"*TAL{i+1} - {char.tal[i]}*\n")
   printable += (f"HP - {char.hp}/{char.max_hp}\n")
@@ -74,12 +90,31 @@ def printchar(char: Character):
 
 db["359489732134305793"] = char_to_list(jack_char)
 db["531288319859097601"] = char_to_list(felix_char)
+db["962543637445615656"] = char_to_list(mike_char)
 db["262320046653702145"] = char_to_list(my_char)
+
+char_cache = {}
+
+def find_char(id: str):
+  print(f"Finding character for {id}")
+  if id in char_cache:
+    return char_cache[id]
+  elif id in db:
+    char_cache[id] = list_to_char(db[id])
+    return char_cache[id]
+  else:
+    return False
+
+def save_char(char: Character):
+  char_list = char_to_list(char)
+  db[char.id] = char_list
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+greetings_back = ["Hello!", "Hi!", "Good to see you!", "Howdy!"]
 
 @client.event
 async def on_ready():
@@ -90,9 +125,16 @@ async def on_message(message):
   if message.author == client.user:
     return
   id = str(message.author.id)
-  msg = message.content
+  msg = message.content.upper()
+      
   if msg.startswith('9..hello'):
-    await message.channel.send('Hello!')
+    await message.channel.send(random.choice(greetings_back))
+
+  if "LOVE" in msg or "ILY" in msg and "NINE" in msg and id == names['MIKEY']:
+    await message.channel.send("I love you too Mikey!")
+
+  if "THANKS" in msg or "THANK YOU" in msg and "NINE" in msg:
+    await message.channel.send("You're welcome!")
 
   if msg.startswith('9..'):
     print("message detected.")
@@ -100,21 +142,45 @@ async def on_message(message):
     if command == '':
       print("There is nothing")
     command = command.split()
+    
+    print(f"Matching command {command[0]}")
     match command[0]:
-      case 'char':
+      case 'CHAR':
         print("character case entered.")
         if len(command) == 1:
-          if id in db:
-            printable = printchar(list_to_char(db[id]))
+          char = find_char(id)
+          if char:
+            printable = printchar(char)
             await message.channel.send(printable)
           else:
             await message.channel.send("Character not found.")
-        elif names[command[1].upper()] in db:
-          printable = printchar(list_to_char(db[names[command[1].upper()]]))
-          await message.channel.send(printable)
+        elif len(command) == 2:
+          char = find_char(names[command[1].upper()])
+          if char:
+            printable = printchar(char)
+            await message.channel.send(printable)
+          else:
+            await message.channel.send("Character not found.")
         else:
-          await message.channel.send("Character not found.")
-        
+          await message.channel.send("Error - cannot parse command")
+
+      case 'HP':
+        print("HP case entered.")
+        if len(command) == 1:
+          char = find_char(id)
+          if char:
+            await message.channel.send(f"HP - {char.hp} / {char.max_hp}")
+          else:
+            await message.channel.send("Character not found.")
+        elif len(command) == 2:
+          char = find_char(id)
+          if char:
+            old_hp = char.hp
+            char.hp += int(command[1])
+            save_char(char)
+            await message.channel.send(f"HP - {old_hp}/{char.max_hp} -> "
+            f"**{char.hp}/{char.max_hp}**")
+
 my_secret = os.environ['TOKEN']
 client.run(my_secret)
 
